0babca2874f48432824bb54be5aeb9d9087f5d3a
Pablo Perez <paperezpablo@gmail.com>
2025-12-16 00:23:13 -0300

    fix: correct ItemList import casing for Netlify

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { ItemList } from "../ItemList/ItemList";
+import { ItemList } from "../ItemList/itemList";
 
 export const ItemListContainer = () => {
   const [products, setProductos] = useState([]);
