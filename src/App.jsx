ab29bbc1e95226351017973f6b68259d250baccc
Pablo Perez <paperezpablo@gmail.com>
2025-12-15 23:50:16 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ItemListContainer } from "./components/ItemListContainer/itemListContainer";
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
 import { CartProvider } from "./context/CartContext/CartProvider";
 import { MainLayout } from "./Layouts/MainLayout";
